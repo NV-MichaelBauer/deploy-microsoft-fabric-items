4e2179f0717b9f0b51271984efc48de7d6a9f326
NV-MichaelBauer <148345603+NV-MichaelBauer@users.noreply.github.com>
2025-11-21 07:51:06 +0100

    Enhance auth_spn_deploy_fabric_items.py with feature flags
    Added feature flag support to the deployment script.

--- scripts/auth_spn_deploy_fabric_items.py
+++ scripts/auth_spn_deploy_fabric_items.py
@@ -7,7 +7,7 @@ Can be expanded to retrieve values from Key Vault or other sources
 """
 # Kevin Chant extended this...
 # START-EXAMPLE
-from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items
+from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items, append_feature_flag
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some variables.')
@@ -15,13 +15,23 @@ parser.add_argument('--WorkspaceId', type=str)
 parser.add_argument('--Environment', type=str)
 parser.add_argument('--RepositoryDirectory', type=str)
 parser.add_argument('--ItemsInScope', type=str)
+parser.add_argument('--FeatureFlags', type=str)
 args = parser.parse_args()
 
 # Convert item_type_in_scope into a list
 allitems = args.ItemsInScope
 item_type_in_scope=allitems.split(",")
-print(item_type_in_scope)
+print("items in scope:", item_type_in_scope)
 
+# Convert fabric_cicd_feature_flags into a list
+featflags = args.FeatureFlags
+featflags = featflags.replace(" ", "")
+fabric_cicd_feature_flags = featflags.split(",")
+
+for flag in fabric_cicd_feature_flags:
+    print("Append feature flag: ", flag)
+    append_feature_flag(flag)
+    
 # Initialize the FabricWorkspace object with the required parameters
 target_workspace = FabricWorkspace(
     workspace_id= args.WorkspaceId,
